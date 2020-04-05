191aa67e11289a217898bf426b16c9c302aade6e
김진민 <isugar150@gmail.com>
2020-04-05 21:43:09 +0900

    웹소켓 서버 만듬

---
+++ DocConvert_API_JAVA/src/Client.java
@@ -0,0 +1,17 @@
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+/**
+ * Java Socket Client
+ */
+public class JavaSocketClient {
+
+    public static void main(String[] args) {
+
+    }
+}
