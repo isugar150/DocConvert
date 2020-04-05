e045f9d0f66327a11c51e757e30a35ff17088037
김진민 <isugar150@gmail.com>
2020-04-05 11:41:53 +0900

    test

--- DocConvert_Server/Forms/Main.Designer.cs
+++ DocConvert_Server/Forms/Main.Designer.cs
@@ -232,8 +232,8 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(1333, 604);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.statusStrip1);
