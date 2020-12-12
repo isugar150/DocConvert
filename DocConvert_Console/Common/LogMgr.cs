f10711cd0b8f0413caaa5880e310d3aa06640715
JinMin KIm <isugar150@gmail.com>
2020-12-12 13:08:34 +0900

    Console버전 추가

---
+++ DocConvert_Console/Common/LogMgr.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocConvert_Console.Common
+{
+    public class LogMgr
+    {
+        public static void ConsoleWrite(string text, ConsoleColor color)
+        {
+            ConsoleColor originalColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+
+            Console.WriteLine(text);
+            Console.Out.Flush();
+
+            Console.ForegroundColor = originalColor;
+        }
+    }
+}
---
+++ DocConvert_Console/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DocConvert_Console.Common;
+
+namespace DocConvert_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogMgr.ConsoleWrite(@"
+          _____              _____                          _   
+         |  __ \            / ____|                        | |  
+         | |  | | ___   ___| |     ___  _ ____   _____ _ __| |_ 
+         | |  | |/ _ \ / __| |    / _ \| '_ \ \ / / _ \ '__| __|
+         | |__| | (_) | (__| |___| (_) | | | \ V /  __/ |  | |_ 
+         |_____/ \___/ \___|\_____\___/|_| |_|\_/ \___|_|   \__|
+          / ____|                                               
+         | (___   ___ _ ____   _____ _ __                       
+          \___ \ / _ \ '__\ \ / / _ \ '__|                      
+          ____) |  __/ |   \ V /  __/ |                         
+         |_____/ \___|_|    \_/ \___|_|  
+                ", ConsoleColor.Green);
+        }
+    }
+}
